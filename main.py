9baf87c1449063342605388c1684525be23a5f99
Rasputin <dasturboboot@gmail.com>
2024-08-04 20:44:08 -0500

    addded word count feature

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import sys
 
-def output(book):
+#Get book filename and what we're doing
+mode = sys.argv[1]
+book = sys.argv[2]
+
+#Output whole book
+def read(book):
 	with open(f"books/{book}") as f:
 		file = f.read()
 		print(file)
 
-output(sys.argv[1])
+def main():
+	with open(f"books/{book}") as f:
+		file = f.read()
+		if mode == "words":
+			print(f"Word count: {len(file.split())}")
+		elif mode == "read":
+			read(book)
+		else:
+			print("Useage is \"python3 main.py MODE BOOK.txt\"")
+main()
